889072bd25ab40d8d6ac49b3d0fca598751f6d7a
Khoa Ho <hokhoa@grinnell.edu>
2017-09-06 21:57:59 -0500

    Add files via upload

---
+++ WordGenerator.java
@@ -0,0 +1,47 @@
+package hw2;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+public class WordGenerator {
+	private Scanner text;
+	private int wordCount;
+	private int sentCount;
+
+	public WordGenerator(String filename) throws IOException {
+		this.text = new Scanner(new File(filename));
+		this.wordCount = 0;
+	}
+
+	public boolean hasNext() {
+		return this.text.hasNext();
+	}
+
+	public String next() {
+		// Regular expression for a word ended with ".", "?", or "!"
+		String puncRegex = "\\w+[.!?]";
+		String nextWord = this.text.next();
+		this.wordCount++;
+		// If there is a match, then the sentence has ended.
+		if (nextWord.matches(puncRegex)) {
+			this.sentCount++;
+		}
+		return nextWord;
+	}
+
+	public int getWordCount() {
+		return this.wordCount;
+	}
+
+	public int getSentCount() {
+		return this.sentCount;
+	}
+
+//	public static void main(String[] args) throws IOException {
+//		WordGenerator g = new WordGenerator("test1.txt");
+//		while (g.hasNext()) {
+//			System.out.println(g.next());
+//		}
+//	}
+}
